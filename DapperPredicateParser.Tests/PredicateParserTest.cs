9e2f461d4b7543553fb774c778681700217936a8
trytocatchme <deadzone747@gmail.com>
2018-06-20 23:33:36 +0200

    add another test

--- DapperPredicateParser.Tests/PredicateParserTest.cs
+++ DapperPredicateParser.Tests/PredicateParserTest.cs
@@ -75,5 +75,49 @@ namespace DapperPredicateParser.Tests
             //Assert
             Assert.AreEqual(result.Count(), testdata.Count());
         }
+
+        [TestMethod]
+        public void Parse_CollectionOfCars_ThreeTypeOfCars()
+        {
+            //Arrange
+            var testdata = Car.PrepareTestData();
+
+            var groupPredicate = new PredicateGroup
+            {
+                Operator = GroupOperator.Or,
+                Predicates = new List<IPredicate>()
+            };
+
+            var predicateGroup1 = new PredicateGroup
+            {
+                Operator = GroupOperator.And,
+                Predicates = new List<IPredicate>()
+                {
+                    Predicates.Field<Car>(f => f.Name, Operator.Eq, "Fiat"),
+                    Predicates.Field<Car>(f => f.Model, Operator.Eq, "Bravo")
+                }
+            };
+
+            var predicateGroup2 = new PredicateGroup
+            {
+                Operator = GroupOperator.And,
+                Predicates = new List<IPredicate>()
+                {
+                    Predicates.Field<Car>(f => f.Cost, Operator.Gt, 20000),
+                    Predicates.Field<Car>(f => f.Type, Operator.Eq, "Sedan"),
+                }
+            };
+
+            groupPredicate.Predicates.Add(Predicates.Field<Car>(f => f.Type, Operator.Eq, "Hatchback"));
+            groupPredicate.Predicates.Add(predicateGroup1);
+            groupPredicate.Predicates.Add(predicateGroup2);
+
+            //Act
+            var expression = PredicateParser<Car>.Parse(groupPredicate).Compile();
+            var result = testdata.Where(expression);
+
+            //Assert
+            Assert.AreEqual(3, result.Count());
+        }
     }
 }
